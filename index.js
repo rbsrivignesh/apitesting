bcc37676480d3ba38c57af78b98d9caa95e08076
rbsrivignesh <100515755+rbsrivignesh@users.noreply.github.com>
2024-05-21 10:09:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const express= require("express");
+const cors= require("cors");
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port =8000;
+
+app.post("/post",(req,res)=>{
+    // console.log("working");
+    // console.log(req.params);
+//     console.log(req.params);
+    // console.log(req);
+console.log(req.query.Qntrl);
+//   console.log("line 14");
+  console.log(req.body);
+//   res.json({message:"request received from postman"});
+// res.json({message:'post working'})
+res.json(req.body);
+})
+app.get("/",(req,res)=>{
+    res.json({message:'hello world'})
+})
+app.listen(port,()=>{
+    console.log(`running on port ${port}`);
+})
