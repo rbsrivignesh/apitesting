52dfa6a30d821edcdaa9f0ad72f8e92b124f469f
rbsrivignesh <100515755+rbsrivignesh@users.noreply.github.com>
2024-06-14 16:20:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ console.log(req.query.Qntrl);
   console.log(req.body);
 //   res.json({message:"request received from postman"});
 // res.json({message:'post working'})
-res.json({"message" : `the paramaeter from post1 is ${req.query.Qntrl}`});
+// res.json({"message" : `the paramaeter from post1 is ${req.query.Qntrl}`});
+res.json({"message" : `the paramaeter from post1 is ${req.query}`});
 })
 app.get("/",(req,res)=>{
     res.json({data : [{name : "Sri Vignesh RB1", age :"1"},{name:"Sri Vignesh RB2", age :"2"},{name: "Sri Vignesh RB3", age:"3"}]} )
